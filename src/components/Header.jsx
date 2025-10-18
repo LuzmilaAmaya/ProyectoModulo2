eb59f7ef5a4b1199a1c5889d112eabda02249700
agustinsc07 <saezagustin75@gmail.com>
2025-10-18 01:14:39 -0300

    se realizo cambios de img en el carusel

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,17 +9,17 @@ export default function Header() {
             desc: "Todo empieza con una subida. Comparti tus canciones y descubri lo que sigue en la musica. ",
         },
         {
-            img: "https://a-v2.sndcdn.com/assets/images/front-hero-artist-fan-534fb484.jpeg",
+            img: "https://i0.wp.com/urbanroosters.news/wp-content/uploads/2020/05/duki-banner-x.jpg?resize=1024%2C550&ssl=1",
             title: "Conecta con millones de oyentes",
             desc: "Crea tu cuenta y hace crecer tu comunidad de fans en todo el mundo.",
         },
         {
-            img: "https://a-v2.sndcdn.com/assets/images/front-hero-artist-fan-534fb484.jpeg",
+            img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj7tiEGF6nrB_2AGYPpVeaya5LkV0tKapKUExpaq-pOuGM-03-Eaxy_6eyK0qStbBJPAiIlr2wIXATntVfuQ5rHi_WtPwvUHm9x4VC-mwLHvIEbbxHNM3PIvO75aSxG-X1Xwlb4cIbxmvxgdpdD0hEF1JjiD2LUmFlqMmmE5881v_2BFxVX8Q6nYgaH_FFo/s1600/YSY%20A%20head.jpg",
             title: "Escucha lo que esta sonando ahora",
             desc: "Descubri musica nueva cada dia, directamente de los artistas que marcan tendencia.",
         },
         {
-            img: "https://a-v2.sndcdn.com/assets/images/front-hero-artist-fan-534fb484.jpeg",
+            img: "https://radiografica.org.ar/wp-content/uploads/2024/05/unnamed-3-1-scaled-1.jpg",
             title: "Creamos el sonido del futuro",
             desc: "Unite a la comunidad y descubri el poder de compartir",
         },
