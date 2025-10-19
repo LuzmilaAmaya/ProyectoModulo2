29198e19ca556b3cee84175f4e6a9d56945a104b
agustinsc07 <saezagustin75@gmail.com>
2025-10-19 00:52:45 -0300

    se realizo la barra de busqueda

---
+++ src/components/Busqueda.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "../css/busqueda.css"
+
+export default function Busqueda() {
+  return (
+    <div className='busqueda'>
+        <input type="text" placeholder='Buscar artistas, canciones...' />
+    </div>
+  );
+}
