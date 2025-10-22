6e1a2434c7e693cf80bfef3f602927ac92b94dc0
Sofia Duran <sofiduran05.2@gmail.com>
2025-10-21 21:18:55 -0300

    Add routes to the proyect

---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <AppNavbar />
+      <div className="container py-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
