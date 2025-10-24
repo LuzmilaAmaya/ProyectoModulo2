8ecff3227e4a507e33a5fa8febb3da37f0ccf1b2
agustinsc07 <saezagustin75@gmail.com>
2025-10-23 23:17:49 -0300

    se arreglo el problema de que al dar click en el boton ver todos no te llevaba a la pagina de artistas

--- src/components/ArtistasPopulares.jsx
+++ src/components/ArtistasPopulares.jsx
@@ -1,23 +1,29 @@
 import React from "react";
-import "../css/artistaspopulares.css"
+import "../css/artistaspopulares.css";
 import artistas from "../js/artistasPopulares";
+import { useNavigate } from "react-router-dom";
 
 export default function ArtistasPopulares() {
+  const navigate = useNavigate();
   return (
     <section className="artistas-populares">
-        <div className="artistas-header">
-            <h2>Artistas populares</h2>
-            <button className="ver-todos-btn">Ver todos</button>
-        </div>
+      <div className="artistas-header">
+        <h2>Artistas populares</h2>
+        <button className="ver-todos-btn" onClick={() => navigate("/artistas")}>
+          Ver todos
+        </button>
+      </div>
 
-        <div className="lista-artistas">
-            {artistas.map((artistas, index) => (
-                <div className="artista" key={index}>
-                    <a href="#"><img src={artistas.imagen} alt={artistas.nombre} /></a>
-                    <p>{artistas.nombre}</p>
-                </div>
-            ))}
-        </div>
+      <div className="lista-artistas">
+        {artistas.map((artistas, index) => (
+          <div className="artista" key={index}>
+            <a href="#">
+              <img src={artistas.imagen} alt={artistas.nombre} />
+            </a>
+            <p>{artistas.nombre}</p>
+          </div>
+        ))}
+      </div>
     </section>
   );
 }
