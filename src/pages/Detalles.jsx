c077e28ce799dcac3b49376961e82c989955506f
lautaro perez <lautaropd019@gmail.com>
2025-10-28 20:06:04 -0300

    agregue la array y modifique el css

--- src/pages/Detalles.jsx
+++ src/pages/Detalles.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
-import "../css/detalles.css"
+import React from 'react';
+import "../css/detalles.css";
+import {canciones} from '../js/arraycanciones'; 
+
 export default function Detalles() {
+  const idcancion = "A1B2C3D4E";  
+  const recomendaciones = canciones.filter(canciones => canciones.genero.toLowerCase().includes("pop"));
   return (
     <div className='musica-page'>
       <section className='musica-detalle'>
@@ -10,18 +14,12 @@ export default function Detalles() {
 
         <div className='albuminfo'>
           <p className='genero'>Trap</p>
-          <h2 className='titulo'>333</h2>
+          <h2 className='titulo'>18</h2>
           <p className='artista'>Yan Block</p>
           
           <div className='detalles'>
-            <p>
-              Duracion: <strong>
-                3:00
-              </strong>
-            </p>
-            <p>
-              ID:plata268
-            </p>
+            <p>Duracion: <strong>3:00</strong></p>
+            <p>id: <strong>{idcancion}</strong></p>
           </div>
 
           <div className='botones'>
@@ -35,49 +33,20 @@ export default function Detalles() {
       <section className='masartistas'>
         <h3>Recomendados <span className='parati'> para ti</span></h3>
         <div className='listacanciones'>
-          <div className='cancion'>
-            <img src="https://imgs.search.brave.com/x4wAI5bkoTmdrCHiw9EtzzGPdbz7THCIyX9jrm7EDI8/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMucW9idXouY29t/L2ltYWdlcy9jb3Zl/cnMvcmIvaHUvYXpk/amtmNWFnaHVyYl82/MDAuanBn" alt="bien o mal" />
-            <p className='genero'>
-              trap
-            </p>
-            <h4>2.0.1.9</h4>
-            <p>
-              Trueno
-            </p>
-            <button>play</button>
-          </div>
-
-          <div className='cancion'>
-            <img src="https://imgs.search.brave.com/x4wAI5bkoTmdrCHiw9EtzzGPdbz7THCIyX9jrm7EDI8/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMucW9idXouY29t/L2ltYWdlcy9jb3Zl/cnMvcmIvaHUvYXpk/amtmNWFnaHVyYl82/MDAuanBn" alt="bien o mal" />
-            <p className='genero'>
-              trap
-            </p>
-            <h4>2.0.1.9</h4>
-            <p>
-              Trueno
-            </p>
-            <button>play</button>
-          </div>
-
-          <div className='cancion'>
-            <img src="https://imgs.search.brave.com/x4wAI5bkoTmdrCHiw9EtzzGPdbz7THCIyX9jrm7EDI8/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMucW9idXouY29t/L2ltYWdlcy9jb3Zl/cnMvcmIvaHUvYXpk/amtmNWFnaHVyYl82/MDAuanBn" alt="bien o mal" />
-            <p className='genero'>
-              trap
-            </p>
-            <h4>2.0.1.9</h4>
-            <p>
-              Trueno
-            </p>
-            <button>play</button>
-          </div>
+          {recomendaciones.map((cancion) => (
+            <div className='cancion' key={cancion.id}>
+              <img src={cancion.img} alt={cancion.titulo} />
+              <p className='genero'>{cancion.genero}</p>
+              <h4>{cancion.titulo}</h4>
+              <p>{cancion.artista}</p>
+              <button>play ▶</button>
+            </div>
+          ))}
         </div>
       </section>
-    </div>
-  );
+    </div>
+  );
 }
-
-
-
     
     
      
