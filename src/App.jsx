e3d5eeed6118a3e0fe2e83d1187c6eec1fec6dd7
Mateo <mnahuelrosas28@gmail.com>
2025-10-28 19:24:50 -0300

    empeze con la parte que me toco que en este caso es la seccion de sobre nosotros

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./components/Header";
 import Busqueda from "./components/Busqueda";
 import ArtistasPopulares from "./components/ArtistasPopulares";
+import SobreNosotros from "./components/sobrenosotros/sobrenosotros.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import TodosLosArtistas from "./components/TodosLosArtistas";
@@ -14,6 +15,7 @@ function App() {
       <Busqueda />
 
       <ArtistasPopulares />
+      <SobreNosotros />
     </>
   )
 }
