297f1cc27a49569c2ba101ed92525603dbb18ad9
Mateo <mnahuelrosas28@gmail.com>
2025-10-30 15:28:45 -0300

    continue trbajando con las descripciones de cada uno , e incluso puse una foto a cada uno, y destaque una frase final

--- src/components/sobrenosotros/sobrenosotros.jsx
+++ src/components/sobrenosotros/sobrenosotros.jsx
@@ -16,61 +16,65 @@ export default function SobreNosotros() {
         <div className="grid">
           <div className="card">
             <img
-              src=""
+              src="https://res.cloudinary.com/dw05hk9ht/image/upload/v1761844680/WhatsApp_Image_2025-10-30_at_2.15.51_PM_xux7fo.jpg"
               alt="foto de agustin"
             />
-            <h3 class="text-center">descripción</h3>
+            <h3 class="text-center">Descripción</h3>
+            <p></p>
+          </div>
+
+          <div className="card">
+            <img src=""
+             alt="foto de lautaro" />
+            <h3>Descripcion</h3>
             <p>
+              Hola, soy Lautaro Pérez, tengo 18 años y estudio Programación en
+              RollingCode. Me apasiona la tecnología y la resolución de
+              problemas. Disfruto aprender cosas nuevas, trabajar con código
+              (aunque a veces lo rompa un poco antes de arreglarlo) y seguir
+              mejorando cada día como desarrollador.
             </p>
           </div>
 
           <div className="card">
             <img
-              src=""
-              alt="foto de lautaro"
+              src="https://res.cloudinary.com/dw05hk9ht/image/upload/v1761844684/WhatsApp_Image_2025-10-28_at_8.28.25_PM_umbcpd.jpg"
+              alt="foto de sofia "
             />
             <h3>Descripcion</h3>
-            <p>
-                Hola, soy Lautaro Pérez, tengo 18 años y estudio Programación en RollingCode. Me apasiona la tecnología y la resolución de problemas. Disfruto aprender cosas nuevas, trabajar con código (aunque a veces lo rompa un poco antes de arreglarlo) y seguir mejorando cada día como desarrollador.
-            </p>
+            <p></p>
           </div>
-
+          <div className="card">
+            <img src="https://res.cloudinary.com/dw05hk9ht/image/upload/v1761847310/WhatsApp_Image_2025-10-30_at_2.17.07_PM_rlvmqi.jpg" alt="foto de luzmila" />
+            <h3>Descripcion</h3>
+            <p>
+              Hola!, soy Luzmila Amaya y tengo 18 años, actualmente me encuentro
+              estudiando programación en RollingCode.
+            </p>
+          </div>{" "}
           <div className="card">
             <img
-             src=""
-             alt="foto de sofia "
+              src="https://res.cloudinary.com/dw05hk9ht/image/upload/v1761844654/WhatsApp_Image_2025-10-30_at_2.16.13_PM_pt49s6.jpg"
+              alt="foto de mateo"
             />
             <h3>Descripcion</h3>
             <p>
-              
-            </p>
-          </div>
-           <div className="card">
-            <img
-             src=""
-             alt="foto de luzmila"
-            />
-            <h3>Descripcion</h3>
-            <p>
-              Hola!, soy Luzmila Amaya y tengo 18 años, actualmente me encuentro estudiando programación en RollingCode.
-            </p>
-          </div> <div className="card">
-            <img
-             src=""
-             alt="foto de mateo"
-            />
-            <h3>Descripcion</h3>
-            <p>
-              Me llamo mateo rosas, tengo 16 añosy estoy en 5to  año, y mientras estoy haciendo el curso de programacion en Rolling Code School
+              Me llamo mateo rosas, tengo 16 años y estoy en 5to año de la
+              secundaria, y mientras estoy haciendo el curso de programacion en
+              Rolling Code School
             </p>
           </div>
         </div>
-      <h3 class="">"Aqui ira escrita la frase decidida por el grupo , sobre la pagina"</h3>
+                 {/* Frase a destacar */}
+        <h3>
+          "Nuestra página es donde la música y la pasión se encuentran,
+          encuentra tu ritmo y encuentrate a ti mismo"
+        </h3>
 
         <div className="contact">
           <h4>Contacto</h4>
           <p>
-            Escríbenos: hola@tunombre.com · Síguenos en redes para las últimas
+            Escríbenos: usuario123"@gmail.com· Síguenos en redes para las últimas
             novedades.
           </p>
         </div>
